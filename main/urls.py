9b6e381ce33556c8439db809ce8ccec0affe1464
Marguerite Ellis <megec@Marguerites-MacBook-Pro.local>
2025-06-23 09:44:51 -0700

    initial commit 6/23

---
+++ main/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('page1/', views.page1, name='page1'),
+    path('page2/', views.page2, name='page2'),
+    path('signup/', views.signup, name='signup'),
+]
---
+++ main/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def page1(request):
+    return render(request, 'page1.html')
+
+def page2(request):
+    return render(request, 'page2.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # redirects to login page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
