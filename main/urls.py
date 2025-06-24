c13e42819cbeefacfe87015352983b7a4f9d5fa9
Marguerite Ellis <megec@Marguerites-MacBook-Pro.local>
2025-06-23 21:38:41 -0700

    test push

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('page1/', views.page1, name='page1'),
     path('page2/', views.page2, name='page2'),
     path('signup/', views.signup, name='signup'),
+    path('page3', views.page3, name='page3')
 ]
